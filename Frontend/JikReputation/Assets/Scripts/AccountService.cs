8a6a3565ac39d703aae9ef17ec8a4ae88ec1a983
1TEFYN1 <vladik31032005@gmail.com>
2024-08-14 17:49:40 +0300

    Reg and Log methods

---
+++ Frontend/JikReputation/Assets/Scripts/AccountService.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccountService : MonoBehaviour
+{
+    [SerializeField] private Text _nickname;
+    [SerializeField] private Text _password;
+
+    private const int NICKNAME_LENGTH = 6;
+    private const int PASSWORD_LENGTH = 8;
+
+    private AccountRepository _accountRepository = new TestAccountRepository();
+
+    public void SignUp()
+    {
+        if (IsValidAccount(_nickname.text, _password.text))
+        {
+            Account account = new Account(_nickname.text, _password.text);
+            _accountRepository.Register(account);
+            Debug.Log($"New Account \n Nickname{_nickname.text} Password{_password.text}");
+        }
+    }
+    public void LogIn()
+    {
+        if (IsValidAccount(_nickname.text, _password.text))
+        {
+            Account account = new Account(_nickname.text, _password.text);
+            _accountRepository.Login(account);
+        }
+    }
+    private bool IsValidAccount(string nickname, string password)
+    {
+        return IsValidNick(nickname) && IsValidPassword(password);
+    }
+    private bool IsValidNick(string nickname)
+    {
+        return nickname != null && nickname.Length >= NICKNAME_LENGTH;
+    }
+    private bool IsValidPassword(string password)
+    {
+        return password != null && password.Length >= PASSWORD_LENGTH;
+    }
+}
---
+++ Frontend/JikReputation/Assets/Scripts/TestRealization/TestAccountRepository.cs
@@ -0,0 +1,12 @@
+public class TestAccountRepository : AccountRepository
+{
+    public Iam Login(Account account)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Register(Account account)
+    {
+        return;
+    }
+}
