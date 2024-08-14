30cc62787ae6adbd20f33d84cb3e09e8db0437b3
Igor-beginner <125693726+Igor-beginner@users.noreply.github.com>
2024-08-14 15:43:39 +0300

    Update DatingRepository.cs

--- Frontend/JikReputation/Assets/Scripts/DatingRepository.cs
+++ Frontend/JikReputation/Assets/Scripts/DatingRepository.cs
@@ -3,4 +3,5 @@ abstract class DatingRepository : JwtAuthorizationRepository
   public abstract int Keep(Person person);
   public abstract void Drop(int id);
   public abstract void Edit(Person person);
+  public abstract List<Person> FetchAll();
 }
