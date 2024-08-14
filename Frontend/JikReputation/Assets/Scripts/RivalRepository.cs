ddc171ddf33d0e2edf72fc85db74853e1b4af6d4
Igor-beginner <125693726+Igor-beginner@users.noreply.github.com>
2024-08-14 15:32:34 +0300

    Update RivalRepository.cs

--- Frontend/JikReputation/Assets/Scripts/RivalRepository.cs
+++ Frontend/JikReputation/Assets/Scripts/RivalRepository.cs
@@ -1,11 +1,4 @@
-abstract class RivalRepository 
+abstract class RivalRepository : JwtAuthorizationRepositry
 {
-  private readonly JwtToken _requestJwtToken;
-
-  public RivalRepository(JwtToken requestJwtToken)
-  {
-    _requestJwtToken = requestJwtToken;
-  }
-
-  abstract List<Rival> fetchAll();
+  abstract List<Rival> FetchAll();
 }
