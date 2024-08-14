c5ec54a557523e8cd593d4381c944e3aeb5817b5
Igor-beginner <125693726+Igor-beginner@users.noreply.github.com>
2024-08-14 15:39:26 +0300

    Update Iam.cs

--- Frontend/JikReputation/Assets/Scripts/Iam.cs
+++ Frontend/JikReputation/Assets/Scripts/Iam.cs
@@ -3,11 +3,13 @@ public class Iam
   private string _name { get; set; }
   private LevelOfAcquaintance _level { get; set; } 
   private List<Person> _contacts { get; set; }
-
-  public Iam(string name, LevelOfAcquaintance level, List<Person> contacts)
+  private JwtToken _jwtToken;
+  
+  public Iam(string name, LevelOfAcquaintance level, List<Person> contacts, JwtToken jwtToken)
   {
     _name = name;
     _level = level;
-    _contacts = contacts
+    _contacts = contacts;
+    _jwtToken = jwtToken;
   }
 }
